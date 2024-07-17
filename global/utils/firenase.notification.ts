1bd859e47445a33723a7e408a4020681db373004
Dennis <sd4live@gmail.com>
2024-07-17 16:58:22 +0100

    Update firenase.notification.ts

--- global/utils/firenase.notification.ts
+++ global/utils/firenase.notification.ts
@@ -10,8 +10,7 @@
 
 // exports.notification = async (title, message, userId, type) => {
 //   const registrationToken =
-//     "cEZWzaUQQcaS6vtVe7xhfV:APA91bE1JvrY2xt6v00x3i-rpQWWS4Md0Oaftlk1r9jAlx_eEC6OsM0j5hX2UEpeH2huiS2HVRhQEeEIc8_j-g0n3VebA2359JTSYBHRWbWr472zdhBHCg1MBCIsFZp-7yxwMxvZoFR2";
-
+//   
 //   const messageData = {
 //     notification: { title: title, body: message, type: type },
 //     data: {
